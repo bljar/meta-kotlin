651f845e20e1c47b4aaa71e5b9bf2da9811ec624
Belajar <bljkmp@gmail.com>
2023-06-06 07:32:43 +0700

    Create and print the total and average scores

--- m1l1-practice-using-math-operations-in-kotlin/src/main/kotlin/Main.kt
+++ m1l1-practice-using-math-operations-in-kotlin/src/main/kotlin/Main.kt
@@ -2,4 +2,9 @@ fun main(args: Array<String>) {
     val scoreLevel1 = 79
     val scoreLevel2 = 92
     val scoreLevel3 = 86
+
+    val totalScore = scoreLevel1 + scoreLevel2 + scoreLevel3
+    val averageScore = totalScore / 3
+    println(totalScore)
+    println(averageScore)
 }
