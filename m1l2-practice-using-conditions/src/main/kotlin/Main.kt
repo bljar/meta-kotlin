c58ea15d882351667f7bf966dc788a38552da309
Belajar <bljkmp@gmail.com>
2023-06-06 09:51:02 +0700

    Task 1: Use if-else-if, to print opening hours

--- m1l2-practice-using-conditions/src/main/kotlin/Main.kt
+++ m1l2-practice-using-conditions/src/main/kotlin/Main.kt
@@ -1,4 +1,9 @@
 fun main() {
     val weekday = "Friday"
     println("On $weekday, the opening hours are:")
+    if (weekday == "Monday") {
+        println("8 AM to 12 PM")
+    } else if (weekday == "Tuesday") {
+        println("8 AM to 6 PM")
+    }
 }
