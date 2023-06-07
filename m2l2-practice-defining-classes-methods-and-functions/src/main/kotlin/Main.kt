bc84f47bc436e90a8bb5b19dca80d1142d3a64bc
Belajar <bljkmp@gmail.com>
2023-06-07 17:03:10 +0700

    Completed exercise

--- m2l2-practice-defining-classes-methods-and-functions/src/main/kotlin/Main.kt
+++ m2l2-practice-defining-classes-methods-and-functions/src/main/kotlin/Main.kt
@@ -5,10 +5,53 @@ class Player(val name: String, val surName: String) {
         return "$name $surName"
     }
 }
+class Scores() {
+    fun checkBest(best: Int, current: Int): Int {
+        if(best < current) {
+            return current;
+        }
+        return best;
+    }
+}
 
 fun main() {
     val P1 = Player("Nicola", "Tesla")
     val P2 = Player("Thomas", "Edison")
+    val scoring = Scores()
+    var lvlScore = 0
 
-    println(P1.fullName())
+    // level 1
+    lvlScore = 12
+    P1.totalScore += lvlScore
+    P1.personalBestScore = scoring.checkBest(P1.personalBestScore, lvlScore)
+
+    lvlScore = 34
+    P2.totalScore += lvlScore
+    P2.personalBestScore = scoring.checkBest(P2.personalBestScore, lvlScore)
+
+    // level 2
+    lvlScore = 56
+    P1.totalScore += lvlScore
+    P1.personalBestScore = scoring.checkBest(P1.personalBestScore, lvlScore)
+
+    lvlScore = 78
+    P2.totalScore += lvlScore
+    P2.personalBestScore = scoring.checkBest(P2.personalBestScore, lvlScore)
+
+    // level 3
+    lvlScore = 99
+    P1.totalScore += lvlScore
+    P1.personalBestScore = scoring.checkBest(P1.personalBestScore, lvlScore)
+
+    lvlScore = 10
+    P2.totalScore += lvlScore
+    P2.personalBestScore = scoring.checkBest(P2.personalBestScore, lvlScore)
+
+    if (P1.totalScore > P2.totalScore) {
+        println("The winner is "+ P1.fullName() +  " with a Score of  "+P1.totalScore);
+        println("Personal Best Score is = " +P1.personalBestScore);
+    } else {
+        println("The winner is "+ P2.fullName() + " with a Score of "+P2.totalScore);
+        println("Personal Best Score is = " +P2.personalBestScore);
+    }
 }
