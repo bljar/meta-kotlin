f5f0556e2759620541c1f04093b820b710bb6397
Belajar <bljkmp@gmail.com>
2023-06-07 11:29:50 +0700

    Completed task 3: Using loops

--- m1l3-practice-creating-loops/src/main/kotlin/Main.kt
+++ m1l3-practice-creating-loops/src/main/kotlin/Main.kt
@@ -11,13 +11,37 @@ fun task1() {
         println()
     }
 }
-fun main() {
+fun task2() {
     for (i in 1..5) {
         val numberOfSpaces = i
         for (j in 1..numberOfSpaces) {
             print(" ")
         }
         val numberOfStars = 11 - (i * 2)
+        for (j in 1.. numberOfStars) {
+            print("*")
+        }
+        println()
+    }
+}
+fun main() {
+    for (i in 1..5) {
+        val numberOfSpaces = 6-i
+        for (j in 1..numberOfSpaces) {
+            print(" ")
+        }
+        val numberOfStars = i-1
+        for (j in 1.. numberOfStars) {
+            print("*")
+        }
+        println()
+    }
+    for (i in 1..5) {
+        val numberOfSpaces = i - 1
+        for (j in 1..numberOfSpaces) {
+            print(" ")
+        }
+        val numberOfStars = 6 - i
         for (j in 1.. numberOfStars) {
             print("*")
         }
