0dca37a7e241e9dd99e349b1cb69bcc11c84bb5f
Belajar <bljkmp@gmail.com>
2023-06-07 11:44:42 +0700

    Implement the function

--- m2l1-practice-using-functions-with-results/src/main/kotlin/Main.kt
+++ m2l1-practice-using-functions-with-results/src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 //Add your function below this line
-fun getPoints(basePoints: Int, boost: Int): Int { }
+fun getPoints(basePoints: Int, boost: Int): Int {
+    return basePoints * boost
+}
 fun main() {
     var score = 0;
     println(score)
