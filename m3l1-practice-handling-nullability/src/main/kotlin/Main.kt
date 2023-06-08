849e31e071388a7c2237d2714f75a2a229d98749
Belajar <bljkmp@gmail.com>
2023-06-08 10:32:44 +0700

    Create a Student class and student list

---
+++ m3l1-practice-handling-nullability/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+class Student(
+    val fullName: String,
+    val grade: Double,
+    val id: Int
+)
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
