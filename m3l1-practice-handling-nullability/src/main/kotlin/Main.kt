40a80ccdf0eea3bc6cafe46d31b0f2e5a04055bf
Belajar <bljkmp@gmail.com>
2023-06-08 12:30:21 +0700

    completed exercise

--- m3l1-practice-handling-nullability/src/main/kotlin/Main.kt
+++ m3l1-practice-handling-nullability/src/main/kotlin/Main.kt
@@ -1,14 +1,31 @@
-class Student(
+data class Student(
     val fullName: String,
     val grade: Double,
     val id: Int
 )
 
 fun main(args: Array<String>) {
+    // step 1
     val students = listOf<Student>(
         Student("John", 140.0, 223),
         Student("Mark", 120.0, 548),
         Student("Natali", 150.0, 342),
         Student("Sara", 130.0, 781)
     )
+    // step 2
+    fun getStudentById(id: Int): Student {
+        return students.find { it.id==id }!!
+    }
+    // step 3
+    fun searchInStudents(name: String): Student? {
+        return students.find { it.fullName.lowercase() == name.lowercase()}
+    }
+    // step 4
+    println("Please, Enter the student's ID")
+    val id = readln().toInt()
+    println(getStudentById(id))
+    println("Please, Enter the student's name")
+    val name= readln()
+    println(searchInStudents(name)?:"The student is not found")
+
 }
