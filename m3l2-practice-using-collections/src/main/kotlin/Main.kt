056a8398a6f2b86928f9e73e185927f894df284b
Belajar <bljkmp@gmail.com>
2023-06-09 13:29:48 +0700

    Tidy up

--- m3l2-practice-using-collections/src/main/kotlin/Main.kt
+++ m3l2-practice-using-collections/src/main/kotlin/Main.kt
@@ -1,29 +1,25 @@
-data class Comment(
+class Comment(
     val userId: Int,
     val message: String
 )
 
 fun main() {
-    var comments: List<Comment> = listOf(
+    val comments: List<Comment> = listOf(
         Comment(5241, "Nice code"),
         Comment(6624, "Like it"),
         Comment(5224, "What's doing on?"),
         Comment(9001, "Check out my website"),
         Comment(8818, "Hello everyone, :)")
     )
-    var blockedUserIds: Set<Int> = setOf(5524, 9001)
-    var userIdToRelation: Map<Int, String> = mapOf(
+    val blockedUserIds: Set<Int> = setOf(5524, 9001)
+    val userIdToRelation: Map<Int, String> = mapOf(
         5241 to "Friend",
         6624 to "Work Colleague"
     )
     for (comment in comments) {
         if (comment.userId !in blockedUserIds) {
-            val relation = userIdToRelation[comment.userId]
-            if (relation != null) {
-                println("${relation}: ${comment.message}")
-            } else {
-                println("unknown: ${comment.message}")
-            }
+            val relation: String = userIdToRelation[comment.userId] ?: "unknown"
+            println("Comment ${comment.message} from $relation")
         }
     }
 }
