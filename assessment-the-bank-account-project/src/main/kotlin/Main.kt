a0674739c7d4aa7e3b43755e97a194bf397e40f8
Belajar <bljkmp@gmail.com>
2023-06-10 12:23:45 +0700

    Build the visual user interface (UI) and set up accounts

--- assessment-the-bank-account-project/src/main/kotlin/Main.kt
+++ assessment-the-bank-account-project/src/main/kotlin/Main.kt
@@ -1,7 +1,20 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    println("Welcome to your banking system.\n" +
+            "What type of account would you like to create?\n" +
+            "1. Debit account\n" +
+            "2. Credit account\n" +
+            "3. Checking account")
+    var accountType = ""
+    while (accountType == "") {
+        println("Choose an option (1, 2 or 3)")
+        val userChoice = (1..5).random()
+        println("The selected option is $userChoice")
+        accountType = when (userChoice) {
+            1 -> "debit"
+            2 -> "credit"
+            3 -> "checking"
+            else -> continue
+        }
+    }
+    println("You have created a $accountType account.")
 }
