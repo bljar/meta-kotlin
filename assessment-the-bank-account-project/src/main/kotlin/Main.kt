0e88c9d6a9638472571d2ef2d5481dd77f5d8104
Belajar <bljkmp@gmail.com>
2023-06-10 17:55:28 +0700

    Define bank account operations

--- assessment-the-bank-account-project/src/main/kotlin/Main.kt
+++ assessment-the-bank-account-project/src/main/kotlin/Main.kt
@@ -17,4 +17,71 @@ fun main() {
         }
     }
     println("You have created a $accountType account.")
+    // Task 2.1
+    var accountBalance = (0..1000).random()
+    println("The current balance is $accountBalance dollars.")
+    val money = (0..1000).random()
+    println("The amount you transferred is $money dollars.")
+    // Task 2.2
+    var output: Int
+    // Task 2.3
+    fun withdraw(amount: Int): Int {
+        accountBalance -= amount
+        println("You successfully withdrew $amount dollars. The current balance is $accountBalance dollars.")
+        return amount
+    }
+
+    // Task 2.4
+    output = withdraw(money)
+    println("The amount you withdrew is $output dollars.")
+
+    // Step 2.5
+    fun debitWithdraw(amount: Int): Int {
+        return if (accountBalance == 0) {
+            println("Can't withdraw, no money on this account!")
+            accountBalance
+        } else if (amount > accountBalance) {
+            println("Not enough money on this account! The checking balance is $accountBalance dollars.")
+            0
+        } else {
+            withdraw(amount)
+        }
+
+    }
+
+    // Task 2.6
+    output = debitWithdraw(money)
+    println("The amount you withdrew is $output dollars.")
+
+    // Task 2.7
+    fun deposit(amount: Int): Int {
+        accountBalance += amount
+        println("You successfully deposited $amount dollars. The current balance is $accountBalance dollars.")
+        return amount
+    }
+
+    // Task 2.8
+    output = deposit(money)
+    println("The amount you deposited is $output dollars.")
+
+    // Task 2.9
+    fun creditDeposit(amount: Int): Int {
+        return if (accountBalance == 0) {
+            println("This account is completely paid off! Do not deposit money!")
+            0
+        } else if (amount + accountBalance > 0) {
+            println("Deposit failed, you tried to pay off an amount greater than the credit balance. The checking balance is $accountBalance dollars.")
+            0
+        } else if (amount == -accountBalance) {
+            accountBalance = 0
+            println("You have paid off this account!")
+            amount
+        } else {
+            (deposit(amount))
+        }
+    }
+
+    // Task 2.10
+    output = creditDeposit(money)
+    println("Credit deposit output: $output")
 }
