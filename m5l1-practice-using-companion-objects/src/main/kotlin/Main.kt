96b8bfaebc48e94d860be657f29564f1c30bce65
rafiyq <rafiyq@outlook.com>
2023-06-26 12:43:51 +0700

    Define the class for the tax calculator and its companion object

--- m5l1-practice-using-companion-objects/src/main/kotlin/Main.kt
+++ m5l1-practice-using-companion-objects/src/main/kotlin/Main.kt
@@ -2,3 +2,13 @@ data class OrderItem(
     val name: String,
     val price: Double
 )
+class TaxCalculator {
+    companion object {
+        val salesTaxPercentage = 15.0
+
+        fun getTaxAmountForOrderItems(orderItemList: List<OrderItem>): Double {
+
+        }
+
+    }
+}
