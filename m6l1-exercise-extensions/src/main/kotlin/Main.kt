e1de2ad4900b51ffefcd36c19de8a96d853fb3d0
Belajar <bljkmp@gmail.com>
2023-06-30 10:15:58 +0700

    Print out the list of ingredients

--- m6l1-exercise-extensions/src/main/kotlin/Main.kt
+++ m6l1-exercise-extensions/src/main/kotlin/Main.kt
@@ -2,3 +2,12 @@ class Dish(
     val dishName: String,
     val ingredients: MutableList<String>
 )
+
+fun Dish.printIngredients() {
+    println(this.ingredients)
+}
+
+fun main() {
+    val onionSoup = Dish("Onion Soup", mutableListOf("Onion", "Cheese", "Water", "Salt"))
+    onionSoup.printIngredients()
+}
