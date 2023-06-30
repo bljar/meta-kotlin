add54c65fcce7c0074808f4bee0efb02440adce3
Belajar <bljkmp@gmail.com>
2023-06-30 10:18:51 +0700

    Remove salt from ingredients

--- m6l1-exercise-extensions/src/main/kotlin/Main.kt
+++ m6l1-exercise-extensions/src/main/kotlin/Main.kt
@@ -7,7 +7,12 @@ fun Dish.printIngredients() {
     println(this.ingredients)
 }
 
+fun Dish.removeSalt() {
+    this.ingredients -= "Salt"
+}
+
 fun main() {
     val onionSoup = Dish("Onion Soup", mutableListOf("Onion", "Cheese", "Water", "Salt"))
+    onionSoup.removeSalt()
     onionSoup.printIngredients()
 }
