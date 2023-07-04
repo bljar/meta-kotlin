97fd5f0a0f93b09b0ad4d10863486cec5e452ad0
Belajar <bljkmp@gmail.com>
2023-07-04 08:30:23 +0700

    Defining the button

--- m7l1-practice-defining-listeners-in-android/app/src/main/java/com/example/definelistener/MainActivity.kt
+++ m7l1-practice-defining-listeners-in-android/app/src/main/java/com/example/definelistener/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.definelistener
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val saveDataButton = findViewById<Button>(R.id.saveDataButton)
     }
 }
