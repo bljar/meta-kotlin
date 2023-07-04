4a182f82f1b94b3bfd88fb88998c226770766484
Belajar <bljkmp@gmail.com>
2023-07-04 12:54:32 +0700

    Print each attempt value and the highest of all discount values

--- m7l2-exercise-the-repeat-function/src/main/kotlin/Main.kt
+++ m7l2-exercise-the-repeat-function/src/main/kotlin/Main.kt
@@ -1,7 +1,15 @@
 import kotlin.random.Random
 
 fun main() {
-    repeat(3) {
+    var maximumDiscountValue = 0
+
+    repeat(3) { index ->
         val discount = Random.nextInt(10)
+        println("Attempt ${index+1}: $discount")
+        if (discount > maximumDiscountValue) {
+            maximumDiscountValue = discount
+        }
     }
+
+    println(maximumDiscountValue)
 }
