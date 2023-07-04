74ac278daa6891a584a5770c9475b5151f4a16d6
Belajar <bljkmp@gmail.com>
2023-07-04 19:14:34 +0700

    Create a sample order list

--- m7l3-exercise-process-collections/src/main/kotlin/Main.kt
+++ m7l3-exercise-process-collections/src/main/kotlin/Main.kt
@@ -1,7 +1,15 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+data class Order(
+    val orderId: Int,
+    val month: String,
+    val amount: Int
+)
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val orderList = listOf(
+        Order(1, "August", 40),
+        Order(2, "August", 27),
+        Order(3, "September", 44),
+        Order(4, "September", 57),
+        Order(5, "October", 38),
+    )
 }
